dc4c3be03b7944f0c871e6d86d3956e98b095d96
Hamza Ahmad <hamzahmd.dev@gmail.com>
2022-03-31 14:08:58 +0500

    Configuring MUI and tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
+            <p className="text-red-800">
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
